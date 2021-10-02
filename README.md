272be496f6e21fd98de195bc52c91de9b66ee377
k0t3n <k0t3n.mail@gmail.com>
2021-10-02 18:31:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sentry tunnel
+
+This is an aiohttp-based application for tunnelling Sentry traffic to avoid blocking by adblockers.
+
+## Installation
+```
+pip install -r requirements.txt
+```
+
+## Usage
+```
+python server.py
+```
---
+++ server.py
@@ -0,0 +1,31 @@
+import json
+from json import JSONDecodeError
+
+from aiohttp import ClientSession, web
+
+SENTRY_HOST = 'sentry.io'
+
+
+async def main(request):
+    data = await request.text()
+
+    try:
+        headers, *_ = data.split('\n')
+        dsn = json.loads(headers)['dsn']
+        project_id = int(dsn.split('/')[-1])
+    except (ValueError, JSONDecodeError):
+        return web.Response(text="Invalid request", status=400)
+
+    async with ClientSession() as session:
+        async with session.post(
+                f'https://{SENTRY_HOST}/api/{project_id}/envelope/',
+                data=data,
+        ) as response:
+            response_data = await response.text()
+
+        return web.Response(text=response_data)
+
+
+app = web.Application()
+app.add_routes([web.post('/', main)])
+web.run_app(app)
